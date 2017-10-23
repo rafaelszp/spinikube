33e6fe3e02bfe02a3f883ecfb29761476b083529
Rafael Pereira <rafaelszp.info@gmail.com>
2017-10-23 15:12:21 -0200

    removendo minikube

--- setup.py
+++ setup.py
@@ -30,12 +30,6 @@ def c(cmd):
   o("kubectl create -f " + cmd + " --namespace spinnaker")
   time.sleep(2)
 
-o("minikube delete")
-
-o("minikube start --memory 10000 --cpus 4 --disk-size=60g")
-
-#o("kubectl delete namespace spinnaker")
-#time.sleep(30)
 o("kubectl create namespace spinnaker")
 
 c("applications/kubedash/bundle.yaml")
@@ -58,50 +52,6 @@ for component in components:
 
 poll()
 
-os.system("rm -rf minikube")
-os.system("mkdir minikube")
-os.system("cp ~/.minikube/apiserver.crt minikube/apiserver.crt")
-os.system("cp ~/.minikube/apiserver.key minikube/apiserver.key")
-os.system("cp ~/.minikube/ca.crt minikube/ca.crt")
-
-ip = os.popen('minikube ip').read().strip()
-
-kubeConfig = """
-apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority: /root/.kube/ca.crt
-    server: https://""" + ip + """:8443
-  name: minikube
-contexts:
-- context:
-    cluster: minikube
-    user: minikube
-  name: minikube
-current-context: minikube
-kind: Config
-preferences: {}
-users:
-- name: minikube
-  user:
-    client-certificate: /root/.kube/apiserver.crt
-    client-key: /root/.kube/apiserver.key
-"""
-
-with open("minikube/config", "w") as text_file:
-  text_file.write(kubeConfig)
-
-time.sleep(1)
-
-os.system("kubectl create secret generic spinnaker-config --from-file=./config/echo.yml --from-file=./config/igor.yml --from-file=./config/gate.yml --from-file=./config/orca.yml --from-file=./config/rosco.yml --from-file=./config/front50.yml --from-file=./config/clouddriver.yml --namespace spinnaker")
-
-os.system("kubectl create secret generic minikube-config --from-file=./minikube/config --from-file=./minikube/ca.crt --from-file=./minikube/apiserver.crt --from-file=./minikube/apiserver.key --namespace spinnaker")
-
-os.system("rm -rf minikube")
-
-#print "seeding spinnaking images"
-o("./podexec jenkins /usr/local/jenkins/jobs/seed.sh")
-
 components = ('front50', 'clouddriver', 'rosco', 'orca', 'igor', 'gate', 'deck')
 for component in components:
   c("applications/spinnaker/" + component + "/controller.yml")
@@ -111,57 +61,6 @@ poll()
 
 time.sleep(2)
 
-services = '''
-{
-"services" : [
-
-          {
-    "title": "Spinnaker Dashboard",
-    "description": "Spinnaker UI",
-    "link": "''' + cmdOut("minikube service spinnaker-deck --namespace spinnaker --url") + '''"
-    },
-
-
-
-    {
-    "title": "Kubernetes Dashboard",
-    "description": "Management UI",
-    "link": "''' + cmdOut("minikube service kubernetes-dashboard --namespace kube-system --url") + '''"
-    },
-
-        {
-    "title": "Tectonic Console",
-    "description": "Alternative management UI",
-    "link": "''' + cmdOut("minikube service tectonic --namespace spinnaker --url") + '''"
-    },
-
-
-    {
-    "title": "Jenkins",
-    "description": "Automation Server",
-    "link": "''' + cmdOut("minikube service spinnaker-jenkins --namespace spinnaker --url") + '''"
-    },
-
-        {
-    "title": "Cluster Performace",
-    "description": "Performance analytics UI",
-    "link": "''' + cmdOut("minikube service kubedash --namespace spinnaker --url") + '''"
-    },
-      {
-    "title": "Container Image Registry",
-    "description": "Local image repository",
-    "link": "''' + cmdOut("minikube service spinnaker-registryui --namespace spinnaker --url") + '''"
-    },
-    {
-    "title": "Apt Repository",
-    "description": "Local apt repository",
-    "link": "''' + cmdOut("minikube service spinnaker-debweb --namespace spinnaker --url") + '''"
-    }
-
-]
-}
-'''
-
 os.system("rm -f applications/start/services.json")
 
 with open("applications/start/services.json", "w") as text_file:
@@ -169,17 +68,7 @@ with open("applications/start/services.json", "w") as text_file:
 
 os.system("kubectl create secret generic start-config --from-file=./applications/start/index.html --from-file=./applications/start/services.json --namespace spinnaker")
 
-#cqlsh -e "COPY front50.pipeline TO '/front50.pipeline.csv' WITH HEADER = 'true'"
-
 c("applications/start/deployment.yml")
 c("applications/start/service.json")
 
 poll()
-
-#add example pipeline
-o("./podexec spinnaker apt-get update")
-o("./podexec spinnaker apt-get install -y git")
-o("./podexec spinnaker git clone git@github.com:moondev/SpiniKube.git /SpiniKube")
-o("./podexec spinnaker cqlsh -e 'COPY front50.pipeline FROM \'/SpiniKube/pipelines/pipelines.csv\' WITH HEADER = \'true\';'")
-
-o("minikube service spinnaker-start -n spinnaker")
